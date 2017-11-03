4ffff4fb88bbe7767435097e9de107794db0e90b
Martin Labik <martin.labik@nurunprague.cz>
2017-11-03 14:35:08 +0100

    Databinding (Input/Output)

--- cooking-book/src/app/app.module.ts
+++ cooking-book/src/app/app.module.ts
@@ -7,8 +7,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
