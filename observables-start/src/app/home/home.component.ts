460528ab4e85b264c84f616d03ada1bdab9db68c
Martin Labik <martin.labik@nurunprague.cz>
2017-11-08 17:34:15 +0100

    Custom observable

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,4 +1,9 @@
+import 'rxjs/Rx';
+
 import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +15,32 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe((number: number) => {
+    //   console.log(number);
+    // });
+    const meinObservabul = Observable.create((observer: Observer<string|number>) => {
+      setTimeout(() => {
+        observer.next('First shot');
+      }, 2000);
+      setTimeout(() => {
+        observer.next(6);
+      }, 4000);
+      setTimeout(() => {
+        observer.error('Its fucked');
+      }, 6000);
+    });
+    meinObservabul.subscribe(
+      (data: string) => {
+        console.log(data);
+      },
+      (error: string) => {
+        console.log(error);
+      },
+      () => {
+        console.log('Shots completed');
+      }
+    );
   }
 
 }
