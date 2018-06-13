ac833ad8afcee8c1435c94bf792ae77a479b81fc
Martin Labik <martin.labik@nurunprague.cz>
2018-06-13 16:11:53 +0200

    Separated recipes to new Module

---
+++ authentication-lazy/src/app/recipes/recipes.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { RecipesComponent } from './recipes.component';
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+
+import { RecipesRoutesModule } from './recipes-routing.module';
+
+@NgModule(
+    {
+        declarations: [
+            RecipesComponent,
+            RecipeStartComponent,
+            RecipeListComponent,
+            RecipeDetailComponent,
+            RecipeEditComponent,
+            RecipeItemComponent
+        ],
+        imports: [
+            CommonModule,
+            FormsModule,
+            ReactiveFormsModule,
+            RecipesRoutesModule
+        ]
+    }
+)
+export class RecipesModule {}
